868e334403b72b44704bf9272e6c5640a1fddc1c
Tatiana-Juin <username@users.noreply.github.com>
2026-02-18 11:58:59 +0100

    On peut maintenant exporter un fichier en pdf mais il a un probleme si ca depasse la deuxieme page le contenus est couper . Je suis en train de modifier le code pour avoir comment faire

--- src/components/DataExportPdf.jsx
+++ src/components/DataExportPdf.jsx
@@ -6,6 +6,33 @@ export default function DataExportPdf({printRef,filesList}) {
     const handleDownload = async()=>{
         const element = printRef.current;
         if(!element)  return;
+        // Capture avec html2 canvas
+        const canvas = await html2canvas(element,{
+            // haute résolution - texte lisible meme si on réduit 
+            scale:2,
+            useCORS: true,
+            logging: false,
+        })
+
+        const imgData = canvas.toDataURL('image/png');
+
+        // Préparation du pdf (A4)
+        const pdf = new jsPDF("p","mm","a4");
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = pdf.internal.pageSize.getHeight();
+
+        // calcule du ration pour ne pas déformer l'image 
+        const imgProps = pdf.getImageProperties(imgData);
+        const ratio = imgProps.width / imgProps.height;
+
+        // Ajuste la largeur de l'image a la largeur du pdf et on enleve les marge
+        const margin = 10;
+        const actualPdfWidth = pdfWidth - (margin * 2);
+        const imgHeightInPdf = actualPdfWidth / ratio;
+
+        // Ajout de l'image 
+        pdf.addImage(imgData,'PNG',margin,margin,actualPdfWidth,imgHeightInPdf);
+        pdf.save("export-donnees.pdf");
     }
 
     // pour ne pas afficher le bouton exporter s'il a aucun fichier 
