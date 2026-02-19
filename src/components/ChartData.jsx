2a4009e6bbde0875a139c3495bddb827e8aa152e
Tatiana-Juin <username@users.noreply.github.com>
2026-02-19 17:39:04 +0100

    J'ai fait le system de toggle

--- src/components/ChartData.jsx
+++ src/components/ChartData.jsx
@@ -1,7 +1,77 @@
 
 
-export default function ChartData() {
+export default function ChartData({file,setFilesList}) {
+
+    const toogleChartConfig = () =>{
+        setFilesList(prevFiles =>
+            prevFiles.map(f =>{
+                if(f.id === file.id){
+                    return {...f,showChartConfig: !f.showChartConfig};
+                }
+                return f;
+            })
+        )
+    }
+
+    //Pour mettre a jours les axeX et Y => pour recuperer la valeur 
+    const updateAxis = (axisName,newValue) =>{
+        setFilesList(prevFiles =>
+            prevFiles.map(f =>{
+                if(f.id === file.id){
+                    return {
+                        ...f,
+                        [axisName] : newValue
+                    }
+                }
+                return f;
+            })
+        )
+    }
   return (
-    <div>ChartData</div>
+    <>
+         <div style={{ margin: '10px 0', padding: '10px', border: '1px solid #eee' }}>
+            {/* BOUTON  */}
+            <button onClick={toogleChartConfig} style={{ cursor: 'pointer', padding: '8px' }}>
+                {file.showChartConfig ? "❌ Fermer les réglages" : "📊 Créer un graphique"}
+            </button>
+
+            {/* On voie le bouton que si c'est true  */}
+            {file.showChartConfig && (
+                <div style={{ marginTop: '15px', background: '#f9f9f9', padding: '10px' }}>
+                    <h4>Configuraion des axes</h4>
+                    <div>
+                            {/* Axe X */}
+                        <label htmlFor=""> Axe X</label>
+                        <select 
+                            value={file.selectedX}
+                            onChange={(e) => updateAxis("selectedX",e.target.value)}
+                        >
+                            <option value=""> ----- Choisir une colonne---</option>
+                            {file.columns.map((col,i) =>(
+                                <option key={i} value={col.name}>{col.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                 
+
+                    {/* AXE Y  */}
+                    <div style={{marginTop:"10px"}}>
+                         <label htmlFor=""> Axe Y</label>
+                        <select value={file.selectedY}
+                            onChange={(e) => updateAxis("selectedY",e.target.value)}
+                        >
+                            <option value=""> ----- Choisir une colonne---</option>
+                            {file.columns.map((col,i) =>(
+                                <option key={i} value={col.name}>{col.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                   
+
+                </div>
+            )}
+        </div>
+    </>
+   
   )
 }
--- src/components/FileEntry.jsx
+++ src/components/FileEntry.jsx
@@ -1,6 +1,7 @@
 
 import DataTable from "./DataTable"
 import FilterData from "./FilterData"
+import ChartData from "./ChartData"
 export default function FileEntry({file,setFilesList}) {
     
   return (
@@ -9,6 +10,8 @@ export default function FileEntry({file,setFilesList}) {
         <h2 style={{ color: "#333" }}>Fichier : {file.name}</h2>
         <FilterData file={file} setFilesList={setFilesList} />
         
+        {/* Pour le graphique */}
+        <ChartData file={file} setFilesList={setFilesList} />
         <DataTable rows={file.rows} columns={file.columns} />
     </div>
   )
